e6b58a4d6660f4a811180bb7a588afd9600df98d
eflipe <heyheymycode@gmail.com>
2025-03-02 15:50:20 -0300

    [ADD] CRUD metods

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # todo_list_owl
 Odoo 17 OWL
 
+Fuentes:
+
+OWL Javascript Framework - Action and Service (Todo List App): https://www.youtube.com/watch?v=Iu99Ytt_-OY
--- static/src/components/todo_list/todo_list.js
+++ static/src/components/todo_list/todo_list.js
@@ -3,7 +3,7 @@
 import { registry } from '@web/core/registry';
 // const { Component, useState} = owl;
 // Probar
-import { Component, useState, onWillStart } from "@odoo/owl";
+import { Component, useState, onWillStart, useRef } from "@odoo/owl";
 import { useService } from "@web/core/utils/hooks";
 
 export class OwlTodoList extends Component {
@@ -11,25 +11,68 @@ export class OwlTodoList extends Component {
 
     setup() {
         this.state = useState({ 
+            task:{name:"Tarea nueva", color:"", completed:false},
             taskList:[
-            ]
+            ],
+            isEdit:false,
+            activeId:false,
         });
-        console.log("Valor inicial:", this.state.value);
         console.log("Valor tasklist:", this.state.taskList);
         // Enlazar el contexto de onToggleCompleted
         this.onToggleCompleted = this.onToggleCompleted.bind(this);
         this.orm = useService("orm")
         this.model = "owl.todo.list"
+        this.searchInput = useRef("search-input")
 
         onWillStart(async ()=>{
             await this.getAllTasks()
         })
-
         
     }
+
+    addTask(){
+        this.resetForm()
+        this.state.isEdit = false
+        this.state.activeId = false        
+    }
+
+    editTask(task){
+        console.log("Valor inicial:", task);
+        this.state.isEdit = true
+        this.state.activeId = task.id
+        this.state.task = {...task}
+    }
+
+    resetForm(){
+        this.state.task = {name:"Tarea nueva", color:"", completed:false}
+    }
+
     async getAllTasks(){
         this.state.taskList = await this.orm.searchRead(this.model, [], ["name", "color", "completed"])
     }
+
+    async saveTask(){
+
+        if (!this.state.isEdit){
+            await this.orm.create(this.model, [this.state.task])
+            this.resetForm()
+        } else {
+            await this.orm.write(this.model, [this.state.activeId], this.state.task)
+        }
+
+        await this.getAllTasks()
+    }
+
+    async deleteTask(task){
+        await this.orm.unlink(this.model, [task.id])
+        await this.getAllTasks()
+    }
+
+    async searchTasks(){
+        const text = this.searchInput.el.value        
+        this.state.taskList = await this.orm.searchRead(this.model, [['name','ilike',text]], ["name", "color", "completed"])
+    }
+
     // Método para manejar el cambio de estado del checkbox
     onToggleCompleted(ev, taskId) {
         // Buscar la tarea en taskList por su ID
@@ -38,7 +81,7 @@ export class OwlTodoList extends Component {
         if (task) {
             // Actualizar el estado de la tarea
             task.completed = ev.target.checked;
-            console.log("Tarea actualizada:", task);
+            console.log("metodo onToggleCompleted Tarea actualizada:", task);
 
     }
 }
